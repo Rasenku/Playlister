72d98f6377bbb5c51e02d081119bc333b445e6d1
Dahveyea <dahveyea.cowan@gmail.com>
2019-12-02 20:47:38 -0800

    added server

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
-clinet = MongoClient()
-db = clinet.Playlister
-
+# Add the following import
+import os
+...
+# update the client, db, and playlists assignments to the following,
+# including the new host variable
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=host)
+db = client.get_default_database()
 playlists = db.playlists
 
 
