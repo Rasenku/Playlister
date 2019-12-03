5eb35b45f09e3c7b745a45320b2003740b4dc39c
Dahveyea <dahveyea.cowan@gmail.com>
2019-12-02 22:48:21 -0800

    Fixed Internal Server error I hope

--- app.py
+++ app.py
@@ -29,34 +29,11 @@ def playlists_submit():
         'video_ids': video_ids
     }
     playlists.insert_one(playlist)
-    return redirect(url_for('playlists_show'))
+    return redirect(url_for('playlists_index'))
 
-
-
-
-
-
-
-
-@app.route('/')
-def index():
-    """Return homepage."""
-    # change the original return statement you wrote to the one below
-    return render_template('home.html', msg='Flask is Cool!!')
 
 
 app = Flask(__name__)
-
-
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
-
-# playlists = [
-#   { 'title': 'Great Playlist' },
-#   { 'title': 'Next Playlist' }
-# ]
 
 
 
@@ -70,19 +47,52 @@ def playlists_index():
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
-    return render_template('playlists_new.html')
+    playlist = {
+        'title': "",
+        'description': "",
+        'videos': "",
+        'video_ids': ""
+    }
+    return render_template('playlists_new.html',playlist=playlist)
 
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
     """Submit a new playlist."""
+    # Grab the video IDs and make a list out of them
+    video_ids = request.form.get('video_ids').split()
+    # call our helper function to create the list of links
+    videos = video_url_creator(video_ids)
     playlist = {
         'title': request.form.get('title'),
-        'description': request.form.get('description')
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
     }
     playlists.insert_one(playlist)
-    return redirect(url_for('playlists_show'))
+    return redirect(url_for('playlists_index'))
 
+
+
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist,comments=playlist_comments)
+
+
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    print(playlist)
+    video_ids = "\n".join(playlist['video_ids'])
+    # Add the title parameter here
+    title = request.form.get('title')
+    return render_template('playlists_edit.html', playlist=playlist, video_ids=video_ids, title='Edit Playlist')
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
@@ -103,29 +113,19 @@ def playlists_update(playlist_id):
     # take us back to the playlist's show page
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
-@app.route('/playlists/<playlist_id>/edit')
-def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    # Add the title parameter here
-    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
-
-
-@app.route('/playlists/<playlist_id>')
-def playlists_show(playlist_id):
-    """Show a single playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    # Add the below line:
-    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
-    # Edit the return statement to be the following:
-    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
-
 
 @app.route('/playlists/comments', methods=['POST'])
 def comments_new():
     """Submit a new comment."""
     # TODO: Fill in the code here to build the comment object,
     # and then insert it into the MongoDB comments collection
+    my_comments = request.form.get('comments').split()
+    comment = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+    }
+    comments.insert_one(comment)
+    print(comment)
     return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
 
